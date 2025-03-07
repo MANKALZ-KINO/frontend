f34c72d5472dc82a8d513be046b416f23a8f7b77
Aluna0001 <lusz0001@stud.kea.dk>
2025-03-07 21:01:24 +0100

    MoviePlan + CSS for MoviePlan and login Mock

--- js/ddChooseMovie.js
+++ js/ddChooseMovie.js
@@ -3,6 +3,41 @@ console.log("Jeg er i ddChooseMovie");
 
 const ddMovies = document.getElementById("ddMovies");
 const ddGenre = document.getElementById("ddGenre");
+const testBody = document.getElementById("body-container");
+
+// function renderBody() {
+//     if (isUserAuthenticated(auth)) {
+//         testBody.innerHTML = `
+//             <select id="ddTeater">
+//                 <option value="">Vælg sal</option>
+//             </select>
+//
+//             <select id="ddDato">
+//                 <option value="">Vælg dato</option>
+//             </select>
+//             <select id="ddMovies">
+//                 <option value="">Vælg film</option>
+//             </select>
+//
+//             <select id="ddGenre">
+//                 <option value="">Vælg genre</option>
+//             </select>
+//         `
+//         return;
+//     }
+//
+//
+//     testBody.innerHTML = `
+//         Opdater siden :)
+//     `
+// }
+
+function isUserAuthenticated() {
+    // TODO: REMOVE THIS
+    localStorage.setItem("auth", 'bruger')
+    const auth = localStorage.getItem("auth");
+    return auth === "bruger";
+}
 
 async function fetchMovies() {
     try {
@@ -27,7 +62,7 @@ async function fetchMovies() {
                 option.value = movie.movieId;
                 ddMovies.appendChild(option);
             });
-            fetchMovieGenre(movies);
+            await fetchMovieGenre(movies);
         } else {
             // Hvis responsen ikke er OK, log fejl
             console.error("Failed to fetch movies: " + response.statusText);
@@ -139,7 +174,7 @@ async function fetchMoviePlan(movieID) {
         if (response.ok) {
             const moviePlans = await response.json();
             console.log("Movie plans received:", moviePlans);
-            displayMoviePlans(moviePlans); //
+            displayMoviePlans(moviePlans, movieID); //
         } else {
             console.error("Failed to fetch movie plan: " + response.statusText);
         }
@@ -149,10 +184,17 @@ async function fetchMoviePlan(movieID) {
 }
 
 
-function displayMoviePlans(moviePlans) {
+function displayMoviePlans(moviePlans, movieId) {
     const moviePlanContainer = document.getElementById("moviePlan");
 
     moviePlanContainer.innerHTML = '';
+
+    if (isUserAuthenticated()) {
+        const moviePlanCreateButton = document.createElement("button");
+        moviePlanCreateButton.textContent = 'Add movie plan';
+        document.getElementById('moviePlan').appendChild(moviePlanCreateButton);
+        moviePlanCreateButton.addEventListener("click", () => openModal('create-movie-plan-modal'));
+    }
 
     moviePlans.forEach(plan => {
         const moviePlanDate = document.createElement('h3');
@@ -165,6 +207,50 @@ function displayMoviePlans(moviePlans) {
     });
 }
 
+// open modal by id
+function openModal(id) {
+    document.getElementById(id).classList.add('open');
+    document.body.classList.add('jw-modal-open');
+}
+
+// close currently open modal
+function closeModal() {
+    document.querySelector('.jw-modal.open').classList.remove('open');
+    document.body.classList.remove('jw-modal-open');
+}
+
+async function saveMoviePlan() {
+    const date = document.getElementById('new-movie-plan-date').value
+    const theater = document.getElementById('new-movie-plan-theater').value
+    const showtime = document.getElementById('new-movie-plan-showing').value
+
+    const dto = {
+        date,
+        theater,
+        showtime
+    }
+
+    const urlMoviePlan = `http://localhost:8080/movieplans`;
+    try {
+        const response = await fetch(urlMoviePlan, {
+            method: 'POST',
+            body: JSON.stringify(dto),
+
+        });
+    } catch (error) {
+        console.error("Error creating movie plan:", error);
+    }
+}
+
+window.addEventListener('load', function() {
+    // close modals on background click
+    document.addEventListener('click', event => {
+        if (event.target.classList.contains('jw-modal')) {
+            closeModal();
+        }
+    });
+});
+
 // Event listener for movie genre selection
 function selectGenre(ev) {
     console.log(ev);
@@ -175,6 +261,7 @@ function selectGenre(ev) {
 }
 
 // Kald fetchMovies når DOM er klar
+// document.addEventListener("DOMContentLoaded", renderBody);
 document.addEventListener("DOMContentLoaded", fetchMovies);
 document.addEventListener("DOMContentLoaded", fetchMovieGenre);
 // Event listener for ændringer i dropdown
