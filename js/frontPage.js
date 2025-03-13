4a893a6ce830b78a949c27adcb738f6c93ad8a6e
“zuunun” <zuun0001@stud.kea.dk>
2025-03-13 12:03:15 +0100

    små ret

--- js/frontPage.js
+++ js/frontPage.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-async function displayMovies(movies) {
+export async function displayMovies(movies) {
     const frontPageContainer = document.getElementById("frontPage");
 
     // Clear the front page container before adding new movies
